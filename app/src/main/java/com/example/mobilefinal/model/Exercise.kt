e6f4b0dcbc8cb69f94ff253a506a77b1c405327f
ohad samu <ohadsamu@gmil.com>
2025-02-01 22:48:31 +0200

    feat: new api for excercises with gif

--- app/src/main/java/com/example/mobilefinal/model/Exercise.kt
+++ app/src/main/java/com/example/mobilefinal/model/Exercise.kt
@@ -2,12 +2,8 @@ package com.example.mobilefinal.model
 
 data class Exercise(
     val id: String = "", // should be reconsidered - looks unused
-//    val imageUrl: String = "", // should be reconsidered - maybe from generic list
-//    val duration: Int = 0, // should be reconsidered - maybe 5m for each one for build workout
+    val gifUrl: String = "", // should be reconsidered - maybe from generic list
     val name: String = "", // must have
     val description: String = "", // must have
     var muscle: String = "", // must have
-    val difficulty: String = "", // should be reconsidered - comes from the api and looks unused
-    val equipment: String = "", // should be reconsidered - comes from the api and looks unused
-    val type: String = "", // should be reconsidered - comes from the api and looks unused
     )
