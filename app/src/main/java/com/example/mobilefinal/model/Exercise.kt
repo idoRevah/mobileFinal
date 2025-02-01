022c29545f9e01031811640e06441d2272d7228f
ohad samu <ohadsamu@gmil.com>
2025-02-01 18:07:45 +0200

    feat: excersices api

--- app/src/main/java/com/example/mobilefinal/model/Exercise.kt
+++ app/src/main/java/com/example/mobilefinal/model/Exercise.kt
@@ -1,9 +1,13 @@
 package com.example.mobilefinal.model
 
 data class Exercise(
-    val id: String = "",
-    val name: String = "",
-    val description: String = "",
-    val duration: Int = 0,
-    val imageUrl: String = ""
-)
+//    val id: String = "", // should be reconsidered - looks unused
+//    val imageUrl: String = "", // should be reconsidered - maybe from generic list
+//    val duration: Int = 0, // should be reconsidered - maybe 5m for each one for build workout
+    val name: String = "", // must have
+    val description: String = "", // must have
+    var muscle: String = "", // must have
+    val difficulty: String = "", // should be reconsidered - comes from the api and looks unused
+    val equipment: String = "", // should be reconsidered - comes from the api and looks unused
+    val type: String = "", // should be reconsidered - comes from the api and looks unused
+    )
