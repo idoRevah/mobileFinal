06d261f56dd87538944af9418d4bfe6c45765301
idoRevah <ido.revah@gmail.com>
2025-03-20 14:28:07 +0200

    feat: base model implemented

---
+++ app/src/main/java/com/example/mobilefinal/model/Comment.kt
@@ -0,0 +1,15 @@
+package com.example.mobilefinal.model
+
+import java.time.LocalDateTime
+
+//import com.google.firebase.firestore.DocumentId
+
+data class Comment (
+//    @DocumentId
+    val id: String = "",
+    val workoutId: String = "",
+    val userId: String = "",
+    val imageBase64: String = "",
+    val text: List<String> = emptyList(),
+    val createDateTime: LocalDateTime
+)
