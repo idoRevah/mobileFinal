5951bd9f3ef4a700b272d3a2975cbfe55097c54f
idoRevah <ido.revah@gmail.com>
2025-03-21 14:11:33 +0200

    firebase connection fix

--- app/src/main/java/com/example/mobilefinal/InitApp.kt
+++ app/src/main/java/com/example/mobilefinal/InitApp.kt
@@ -6,6 +6,7 @@ import com.example.mobilefinal.data.MobileFinalDatabase
 import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -17,30 +18,5 @@ public class InitApp: Application() {
         Log.d("InitApp", "Application created")
         MobileFinalDatabase.initDatabase(this)
         FirebaseApp.initializeApp(this)
-        Log.d("ola", FirebaseFirestore.getInstance().app.name)
-
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val collections = FirebaseFirestore.getInstance().().await()
-                collections.forEach { collection ->
-                    Log.d("FirestoreDebug", "Collection found: ${collection.id}")
-                }
-
-                val snapshot = FirebaseFirestore.getInstance()
-                    .collection("workouts")
-                    .get()
-                    .await()
-
-                if (snapshot.isEmpty) {
-                    Log.e("ola", "❌ Firestore collection is EMPTY!")
-                } else {
-                    snapshot.documents.forEach { document ->
-                        Log.d("ola", "Document ID: ${document.id}, Data: ${document.data}")
-                    }
-                }
-            } catch (e: Exception) {
-                Log.e("ola", "Firestore fetch error: ${e.message}")
-            }
-        }
     }
 }
